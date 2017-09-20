7a88a71245f28690eb64fe8b14109a3db50b24c0
Sebastian Perez <sperez5553@gmail.com>
2017-09-19 22:53:57 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://sperez5553:Valpez04@ds139964.mlab.com:39964/softwaredb', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
